56c88c4a663e335e27b69adeb12ccae59a4bbb51
박지윤 <tkrhkfld123@naver.com>
2023-12-11 23:52:50 +0900

    🌱 Add Banner component file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
 import React,{useEffect} from 'react';
 import { movieAction } from '../redux/actions/movieAction';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import Banner from '../components/Banner';
 
 
 const Home = () => {
   const dispatch = useDispatch();
+  const {popularMovies, topRatedMovies, upcomingMovies} = useSelector(state => state.movie)
+
   useEffect(()=>{
     dispatch(movieAction.getMovies())
   },[])
   return (
-    <div>Home</div>
+    <div>
+      <Banner movie={popularMovies.result[0]}/>
+    </div>
   )
 }
 
