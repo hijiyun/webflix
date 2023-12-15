e5180b965ebfb331fb4ba7bc1521cdb6766af925
박지윤 <tkrhkfld123@naver.com>
2023-12-15 23:58:37 +0900

    💄 영화 카드 UI작업

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import Badge from 'react-bootstrap/Badge';
+import { useSelector } from 'react-redux';
+
 
 const MovieCard = ({item}) => {
+  const { genreList } = useSelector(state=>state.movie)
   return (
     <div 
       className='card'
@@ -9,7 +12,10 @@ const MovieCard = ({item}) => {
       height:200}}>
         <div className='overlay'>
           <h1>{item.title}</h1> 
-          <div>{item.genre_ids.map(id=><Badge bg="secondary">{id}</Badge>)}</div>
+          <div>{item.genre_ids.map(id=><Badge bg="secondary">
+            {genreList.find(item => item.id === id).name}
+          </Badge>)}
+          </div>
           <div>
             <span>{item.vote_average}</span>
             <span>{item.adult?<Badge pill bg="danger">19</Badge>:<Badge pill bg="warning" text="dark">all</Badge>}</span>
