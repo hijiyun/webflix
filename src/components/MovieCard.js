6be0947643e2f83e772f0ddc566f667939b0df30
박지윤 <tkrhkfld123@naver.com>
2023-12-16 21:14:17 +0900

    💄 영화 카드 UI 구현

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,21 +6,23 @@ import { useSelector } from 'react-redux';
 const MovieCard = ({item}) => {
   const { genreList } = useSelector(state=>state.movie)
   return (
-    <div 
-      className='card'
-      style={{backgroundImage:"URL(" + `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${item.poster_path}` + ")",
-      height:200}}>
-        <div className='overlay'>
-          <h1>{item.title}</h1> 
-          <div>{item.genre_ids.map(id=><Badge bg="secondary">
-            {genreList.find(item => item.id === id).name}
-          </Badge>)}
+    <div className='card' style={{border:'none'}}>
+      <div 
+        className='card-img'
+        style={{backgroundImage:"URL(" + `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${item.poster_path}` + ")",
+  }}>
+    </div>
+          <div className='overlay'>
+            <h1 className='head'>{item.title}</h1> 
+            <div>{item.genre_ids.map(id=><Badge bg="secondary">
+              {genreList.find(item => item.id === id).name}
+            </Badge>)}
+            </div>
+            <div>
+              <span>{item.vote_average.toFixed(1)}</span>
+              <span>{item.adult?<Badge pill bg="danger">19</Badge>:<Badge pill bg="warning" text="dark">all</Badge>}</span>
+            </div>
           </div>
-          <div>
-            <span>{item.vote_average}</span>
-            <span>{item.adult?<Badge pill bg="danger">19</Badge>:<Badge pill bg="warning" text="dark">all</Badge>}</span>
-          </div>
-        </div>
     </div>
   )
 }
