2f6b1aa75da85ebb85bf6df71279af974c1aaf65
박지윤 <tkrhkfld123@naver.com>
2023-12-19 21:49:43 +0900

    ⚰️ 필요없는 코드 삭제

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '../img/Netflix_Logo.png'
 import { CiSearch } from "react-icons/ci";
 
 function NavScrollExample() {
