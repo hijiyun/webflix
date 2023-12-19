5ef1b2b1a653b88e2c947b37c651079545dac8ad
박지윤 <tkrhkfld123@naver.com>
2023-12-20 01:28:34 +0900

    💄 영화 디테일 이미지 구현

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -9,13 +9,16 @@ const MovieDetail = () => {
   return (
     <div>
       <div className='movie-detail' 
-      style={{backgroundImage:"URL(" + `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${state.backdrop_path}` + ")",
+      style={{backgroundImage:"linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), URL(" + `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${state.backdrop_path}` + ")",
       }}>        
-        <div>
-          포스터 이미지
-        </div>
-        <div>
-          영화 디테일
+        <div className='movie-detail-info'>
+          <div className='movie-detail-poster'
+          style={{backgroundImage:"URL(" + `https://image.tmdb.org/t/p/original/${state.poster_path}` + ")",
+        }}>
+          </div>
+          <div className='movie-detail-list' style={{color:'white'}}>
+            영화 디테일
+          </div>
         </div>
       </div>
       <Footer />
